00c11b765880635a20e452be4037701fae97129b
alumno informatica <alumno@ciis.usil.edu.pe>
2019-11-06 13:48:37 -0500

    version 2.0

---
+++ Models/Contrato.cs
@@ -0,0 +1,13 @@
+namespace LuzHogar.Models
+{
+    public class Contrato
+    {
+        public string NombreCliente{get; set;}
+
+        public string DireccionCliente{get; set;}
+
+        public string NombreProducto{get; set; }
+
+        public float PrecioTotal{get; set;}
+    }
+}
