c1b57f37665730cb5157a5e472e45e2cb332fd5b
alumno informatica <alumno@ciis.usil.edu.pe>
2019-11-20 10:53:44 -0500

    cambios

--- Controllers/MuebleController.cs
+++ Controllers/MuebleController.cs
@@ -53,17 +53,70 @@ namespace LuzHogar.Controllers
             ViewBag.Categorias=categorias;
             if (ModelState.IsValid)
             {
+                
                 _context.Add(x);
                 _context.SaveChanges();
                 TempData["mensaje"]="Mueble agregado con éxito";
                 TempData["tipoTexto"]="text-success";
                 
-                return View();
+                return RedirectToAction("AgregarMueble");
             }
             
             TempData["mensaje"]="Error. No se pudo agregar el mueble";
             TempData["tipoTexto"]="text-danger";
             return View(x);
+        }
+
+        [Authorize(Roles="admin")]
+        public IActionResult ModificarMueble()
+        {
+            var muebles=_context.Muebles
+                        .Include(x => x.Categoria)
+                        .ToList();
+            return View(muebles);
+        }
+
+
+        [Authorize(Roles="admin")]
+        public IActionResult ActualizarMueble(int muebleId)
+        {
+            var mueble=_context.Muebles
+                        .Include(x => x.Categoria)
+                        .Where(x => x.Id==muebleId)
+                        .FirstOrDefault();
+            var categorias=_context.Categorias.OrderBy(c => c.Nombre).ToList();
+            ViewBag.Categorias=categorias;
+            
+            return View(mueble);
+        }
+
+        [Authorize(Roles="admin")]
+        [HttpPost]
+        public IActionResult ActualizarMueble(Mueble x)
+        {
+            var categorias=_context.Categorias.OrderBy(c => c.Nombre).ToList();
+            ViewBag.Categorias=categorias;
+
+            if (ModelState.IsValid)
+            {
+                 var mueble=_context.Muebles
+                        .Include(x => x.Categoria)
+                        .Where(x => x.Id==muebleId)
+                        .FirstOrDefault();
+                TryUpdateModel(x);
+
+                
+                //_context.Update(x);
+                _context.SaveChanges();
+                TempData["mensaje"]="Mueble modificado con éxito";
+                TempData["tipoTexto"]="text-success";
+                
+                return RedirectToAction("ModificarMueble");
+            }
+            
+            TempData["mensaje"]="Error. No se pudo modificar el mueble";
+            TempData["tipoTexto"]="text-danger";
+            return RedirectToAction("ModificarMueble");
         }
 
         
